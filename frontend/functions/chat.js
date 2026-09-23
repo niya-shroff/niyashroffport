9df9efba75428fa2cfbd78d2fd53f5c02e2f12aa
Niya Shroff <102321655+niya-shroff@users.noreply.github.com>
2026-09-23 17:27:29 -0400

    chat function

--- frontend/functions/chat.js
+++ frontend/functions/chat.js
@@ -1,4 +1,4 @@
-exports.handler = async (event, context) => {
+export const handler = async (event, context) => {
   if (event.httpMethod !== "POST") {
     return { statusCode: 405, body: "Method Not Allowed" };
   }
